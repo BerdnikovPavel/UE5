d1adbc1df42cfd40423009f983ee918bc4475fc6
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2023-12-27 09:58:44 +0400

    Test commit

---
+++ Source/CppBase/Public/CppBaseActor.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppBaseActor.generated.h"
+
+// class UStaticMeshComponent;
+
+UENUM(BlueprintType)
+enum class EMovementState : uint8
+{
+	Mobility,
+	Static
+};
+
+USTRUCT(BlueprintType)
+struct FTransformStruct
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CurrentLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator CurrentRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CurrentScale = FVector(1.0f, 1.0f, 1.0f);
+};
+
+UCLASS()
+class CPPBASE_API ACppBaseActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACppBaseActor();
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	FString PlayerName = this->GetName();
+	UPROPERTY(EditInstanceOnly)
+	int EnemyNum = 20;
+	UPROPERTY(EditInstanceOnly)
+	bool IsAlive = true;
+
+	UFUNCTION(BlueprintCallable)
+	void ShowInformation();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+};
